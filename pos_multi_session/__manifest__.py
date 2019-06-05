5545bb70b5885ffd6ac333e661c2e059049ac9d6
David Beal <david.beal@akretion.com>
2019-06-05 17:47:13 +0200

    FIX in multi-cpny cases

--- pos_multi_session/__manifest__.py
+++ pos_multi_session/__manifest__.py
@@ -24,7 +24,7 @@
     ],
     "external_dependencies": {"python": [], "bin": []},
     "data": [
-        "data/pos_multi_session_data.xml",
+        # "data/pos_multi_session_data.xml",
         "security/ir.model.access.csv",
         "views/pos_multi_session_views.xml",
         "multi_session_view.xml"
